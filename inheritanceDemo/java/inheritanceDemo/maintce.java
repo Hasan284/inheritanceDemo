af18f55ede253aa9c4679dcf80090e2527363894
Hasan Enes Kaya <86600207+Hasan284@users.noreply.github.com>
2023-02-11 00:03:49 +0300

    interitance Ornek
    Banka Ornegi inheritance ile

---
+++ inheritanceDemo/java/inheritanceDemo/maintce.java
@@ -0,0 +1,11 @@
+package inheritanceDemo;
+
+public class maintce {
+
+	public static void main(String[] args) {
+		KrediUI krediUI = new KrediUI();
+		krediUI.KrediHesapla(new TarimKrediManager());
+
+	}
+
+}
